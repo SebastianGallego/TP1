f5d37ebec3ef92363cd8dd4c645699a189b879ba
Sebastian Gallego <sebastian.gallego@gmail.com>
2024-07-16 11:12:38 -0300

    Commit-01

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.router();
+
+router.get("/productos");
+
+// Obtener todos los productos
+// Obtener un producto por su id :id
+// Filtrar Productos por nombre :nombre
+// ++ Filtrar Productos por categoria :nombre
+// Agregar un nuevo producto
+// Actualizar precio de un producto
